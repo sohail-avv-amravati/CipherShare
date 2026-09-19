8fc48898fbbf43eb413a804e1a095b16ed723f93
sohail2006-github <sohail20060501@gmail.com>
2026-09-19 22:20:41 +0530

    Clean up scratch files and redundant scripts

--- scripts/fix_index.php
+++
@@ -1,9 +0,0 @@
-﻿<?php
-$f = file_get_contents(__DIR__ . '/../public/index.php');
-$f = str_replace('AuthManager::isLoggedIn()', 'AuthManager::getCurrentUser()', $f);
-// Fix garbled emojis in feature cards
-$f = str_replace('<div style="font-size: 2rem; margin-bottom: 15px;">??</div>', '<div style="font-size: 2rem; margin-bottom: 15px;">&#x1F512;</div>', $f);
-$f = str_replace('<div style="font-size: 2rem; margin-bottom: 15px;">??</div>', '<div style="font-size: 2rem; margin-bottom: 15px;">&#x23F3;</div>', $f);
-$f = str_replace('<div style="font-size: 2rem; margin-bottom: 15px;">???</div>', '<div style="font-size: 2rem; margin-bottom: 15px;">&#x1F6E1;&#xFE0F;</div>', $f);
-file_put_contents(__DIR__ . '/../public/index.php', $f);
-echo "Fixed.\n";
